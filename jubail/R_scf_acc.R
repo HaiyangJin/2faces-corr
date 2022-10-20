aaa9b361f8a5ac7890d55d1d9db1cfbf2f6a3072
Haiyang <hjin317@aucklanduni.ac.nz>
2022-10-20 16:03:56 +0400

    update ACC for SCF

--- jubail/R_scf_acc.R
+++ jubail/R_scf_acc.R
@@ -4,50 +4,43 @@
 # load packages #
 library(lme4)
 library(lmerTest)
-library(optimx)
 
 df_scf <- readRDS("df_scf.rds")
 
-# Part-whole effect ####
+# Standard composite effect ####
 
 ## Zero-correlation-parameter model ####
-# lmm_scf_acc_zcp <- 
+# message("Fitting the zcp model...\noutput: lmm_scf_acc_zcp")
+# lmm_scf_acc_zcp <-
 #   glmer(Correct ~ Alignment + 
 #           (Ali_C || Subject) +
 #           (Ali_C || StimGroup),
 #         df_scf,
 #         family = binomial(link = "logit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_scf_acc_zcp, "lmm_scf_acc_zcp.rds")
 
 ## Extended model ####
-# (This is essentially the maximal model.)
+# message("Fitting the etd model...\noutput: lmm_scf_acc_etd")
+# # (This is essentially the maximal model.)
 # lmm_scf_acc_etd <- 
 #   glmer(Correct ~ Alignment + 
 #           (Ali_C | Subject) +
 #           (Ali_C | StimGroup),
 #         df_scf,
 #         family = binomial(link = "logit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_scf_acc_etd, "lmm_scf_acc_etd.rds")
 
-## Extended model ####
+## Extended model2 ####
+# message("Fitting the etd2 model...\noutput: lmm_scf_acc_etd2")
 # lmm_scf_acc_etd2 <- 
 #   glmer(Correct ~ Alignment + 
 #           (Ali_C | Subject) +
 #           (0 + Ali_C | StimGroup),
 #         df_scf,
 #         family = binomial(link = "logit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_scf_acc_etd2, "lmm_scf_acc_etd2.rds")
 
 ## session information
