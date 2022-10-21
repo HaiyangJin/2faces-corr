b210040e951d25482d02541063efa856694da6bd
Haiyang <hjin317@aucklanduni.ac.nz>
2022-10-21 09:32:48 +0400

    update d for F&I

--- jubail/R_fi_d.R
+++ jubail/R_fi_d.R
@@ -4,13 +4,13 @@
 # load packages #
 library(lme4)
 library(lmerTest)
-library(optimx)
 
 df_fi <- readRDS("df_fi.rds")
 
 # Facilitation and interference effect ####
 
 ## Zero-correlation-parameter model ####
+# message("Fitting the zcp model...\noutput: lmm_fi_d_zcp")
 # lmm_fi_d_zcp <- 
 #   glmer(isSame ~ Congruency * SD + Cue +
 #           (ConInc + IncIso + SD_C + 
@@ -19,31 +19,11 @@ df_fi <- readRDS("df_fi.rds")
 #              ConInc_SD + IncIso_SD || StimGroup),
 #         df_fi,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
-# 
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_fi_d_zcp, "lmm_fi_d_zcp.rds")
 
-## Reduced model ####
-# lmm_fi_d_rdc <- 
-#   glmer(isSame ~ Congruency * Alignment * SD + Cue +
-#           (Ali_C + SD_C + # Con_C + 
-#              Con_SD +  # Con_Ali + Ali_SD +
-#              Con_Ali_SD || Subject) +
-#           (Con_C + Ali_C + SD_C + 
-#              Con_Ali + Con_SD + Ali_SD # + 
-#            || StimGroup), # Con_Ali_SD
-#         df_fi,
-#         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
-# saveRDS(lmm_fi_d_rdc, "lmm_fi_d_rdc.rds")
-
 ## Extended model ####
+# message("Fitting the etd model...\noutput: lmm_fi_d_etd")
 # lmm_fi_d_etd <- 
 #   glmer(isSame ~ Congruency * SD + Cue +
 #           (ConInc + IncIso + SD_C + 
@@ -52,28 +32,24 @@ df_fi <- readRDS("df_fi.rds")
 #              ConInc_SD + IncIso_SD | StimGroup),
 #         df_fi,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_fi_d_etd, "lmm_fi_d_etd.rds")
 
 ## Extended model2 ####
-# lmm_fi_d_etd2 <- 
+# message("Fitting the etd2 model...\noutput: lmm_fi_d_etd2")
+# lmm_fi_d_etd2 <-
 #   glmer(isSame ~ Congruency * SD + Cue +
-#           (SD_C + # ConInc + IncIso + 
+#           (SD_C + # ConInc + IncIso +
 #              ConInc_SD + IncIso_SD | Subject) +
-#           (0 + SD_C + # ConInc + IncIso + 
+#           (0 + SD_C + # ConInc + IncIso +
 #              ConInc_SD + IncIso_SD | StimGroup),
 #         df_fi,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_fi_d_etd2, "lmm_fi_d_etd2.rds")
 
 ## Extended model3 ####
+# message("Fitting the etd3 model...\noutput: lmm_fi_d_etd3")
 # lmm_fi_d_etd3 <-
 #   glmer(isSame ~ Congruency * SD + Cue +
 #           (0 + SD_C + # ConInc + IncIso +
@@ -82,26 +58,8 @@ df_fi <- readRDS("df_fi.rds")
 #              ConInc_SD | StimGroup), # + IncIso_SD
 #         df_fi,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx",
-#                                optCtrl = list(method = "nlminb",
-#                                               starttests = FALSE,
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_fi_d_etd3, "lmm_fi_d_etd3.rds")
-
-## Extended model4 ####
-# lmm_fi_d_etd4 <-
-#   glmer(isSame ~ Congruency * SD + Cue +
-#           (0 + SD_C + # ConInc + IncIso +
-#              ConInc_SD | Subject) + # + IncIso_SD
-#           (0 + SD_C + # ConInc + IncIso +
-#              ConInc_SD | StimGroup), # + IncIso_SD
-#         df_fi,
-#         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx",
-#                                optCtrl = list(method = "nlminb",
-#                                               starttests = FALSE,
-#                                               kkt = FALSE)))
-# saveRDS(lmm_fi_d_etd4, "lmm_fi_d_etd4.rds")
 
 ## session information
 sessionInfo()
