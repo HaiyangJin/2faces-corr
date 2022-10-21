2c451151a3951793843edaf349e4d5970ac7983a
Haiyang <hjin317@aucklanduni.ac.nz>
2022-10-21 10:59:07 +0400

    update d for CCF

--- jubail/R_ccf_d.R
+++ jubail/R_ccf_d.R
@@ -4,13 +4,13 @@
 # load packages #
 library(lme4)
 library(lmerTest)
-library(optimx)
 
 df_ccf <- readRDS("df_ccf.rds")
 
-# Part-whole effect ####
+# Complete composite effect ####
 
 ## Zero-correlation-parameter model ####
+# message("Fitting the zcp model...\noutput: lmm_ccf_d_zcp")
 # lmm_ccf_d_zcp <- 
 #   glmer(isSame ~ Congruency * Alignment * SD + Cue +
 #           (Con_C + Ali_C + SD_C + 
@@ -21,24 +21,11 @@ df_ccf <- readRDS("df_ccf.rds")
 #              Con_Ali_SD || StimGroup),
 #         df_ccf,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
-
-# lmm_ccf_d_zcp <-
-#   glmer(isSame ~ Congruency * Alignment * SD + Cue +
-#           (Con_C + Ali_C + SD_C +
-#              Con_Ali + Con_SD + Ali_SD +
-#              Con_Ali_SD || Subject) +
-#           (Con_C + Ali_C + SD_C +
-#              Con_Ali + Con_SD + Ali_SD +
-#              Con_Ali_SD || StimGroup),
-#         df_ccf,
-#         family = binomial(link = "probit"))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_ccf_d_zcp, "lmm_ccf_d_zcp.rds")
 
 ## Reduced model ####
+# message("Fitting the rdc model...\noutput: lmm_ccf_d_rdc")
 # lmm_ccf_d_rdc <- 
 #   glmer(isSame ~ Congruency * Alignment * SD + Cue +
 #           (Ali_C + SD_C + # Con_C + 
@@ -49,14 +36,12 @@ df_ccf <- readRDS("df_ccf.rds")
 #            || StimGroup), # Con_Ali_SD
 #         df_ccf,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_ccf_d_rdc, "lmm_ccf_d_rdc.rds")
 
 ## Extended model ####
-# lmm_ccf_d_etd <- 
+# message("Fitting the etd model...\noutput: lmm_ccf_d_etd")
+# lmm_ccf_d_etd <-
 #   glmer(isSame ~ Congruency * Alignment * SD + Cue +
 #           (Ali_C + SD_C + # Con_C + 
 #              Con_SD +  # Con_Ali + Ali_SD +
@@ -66,13 +51,11 @@ df_ccf <- readRDS("df_ccf.rds")
 #            | StimGroup), # Con_Ali_SD
 #         df_ccf,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx", 
-#                                optCtrl = list(method = "nlminb", 
-#                                               starttests = FALSE, 
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_ccf_d_etd, "lmm_ccf_d_etd.rds")
 
 ## Extended model2 ####
+# message("Fitting the etd2 model...\noutput: lmm_ccf_d_etd2")
 # lmm_ccf_d_etd2 <-
 #   glmer(isSame ~ Congruency * Alignment * SD + Cue +
 #           (SD_C + # Con_C + Ali_C + 
@@ -83,13 +66,11 @@ df_ccf <- readRDS("df_ccf.rds")
 #            | StimGroup), # Con_Ali_SD
 #         df_ccf,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx",
-#                                optCtrl = list(method = "nlminb",
-#                                               starttests = FALSE,
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_ccf_d_etd2, "lmm_ccf_d_etd2.rds")
 
 ## Extended model3 ####
+# message("Fitting the etd3 model...\noutput: lmm_ccf_d_etd3")
 # lmm_ccf_d_etd3 <-
 #   glmer(isSame ~ Congruency * Alignment * SD + Cue +
 #           (0 + SD_C + # Con_C + Ali_C + 
@@ -100,13 +81,11 @@ df_ccf <- readRDS("df_ccf.rds")
 #            | StimGroup), # Con_Ali_SD
 #         df_ccf,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx",
-#                                optCtrl = list(method = "nlminb",
-#                                               starttests = FALSE,
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_ccf_d_etd3, "lmm_ccf_d_etd3.rds")
 
 ## Extended model4 ####
+# message("Fitting the etd4 model...\noutput: lmm_ccf_d_etd4")
 # lmm_ccf_d_etd4 <-
 #   glmer(isSame ~ Congruency * Alignment * SD + Cue +
 #           (0 + SD_C + # Con_C + Ali_C + 
@@ -117,10 +96,7 @@ df_ccf <- readRDS("df_ccf.rds")
 #            | StimGroup), # Con_Ali_SD
 #         df_ccf,
 #         family = binomial(link = "probit"),
-#         control = glmerControl(optimizer = "optimx",
-#                                optCtrl = list(method = "nlminb",
-#                                               starttests = FALSE,
-#                                               kkt = FALSE)))
+#         control = glmerControl(optCtrl = list(maxfun = 1e7)))
 # saveRDS(lmm_ccf_d_etd4, "lmm_ccf_d_etd4.rds")
 
 ## session information
